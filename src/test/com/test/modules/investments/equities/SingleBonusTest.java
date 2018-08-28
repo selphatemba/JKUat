e3131d5da9a634640fb5262817ae47121119ce17
selpha <selphatemba@gmail.com>
2018-08-28 12:32:06 +0300

    Initial commit

---
+++ src/test/com/test/modules/investments/equities/SingleBonusTest.java
@@ -0,0 +1,24 @@
+package com.test.modules.investments.equities;
+
+import com.JKUat.base.BaseClass;
+import com.JKUat.base.LoginPage;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SingleBonusTest extends BaseClass {
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        init();
+
+    }
+
+
+    @Test(priority = 0)
+    public void login() throws Exception {
+        log.info("=========logging in user ================");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("admin", "admin");
+        log.info("=============successfully logged in===========");
+    }
+}
